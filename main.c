43ea0ef9fb1628a9ee7c07b266457a1cefb387b8
ky <531595359@qq.com>
2016-07-12 22:38:26 +0800

    change

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
+#include "sort.h"
 
 int main(int argc, char** argv) 
 {
-    printf("888888hello\n");
+    int i,n=8;
+    int a[]={3,19,7,4,8,10,1,5};
+    insertsort(a,n);
+    print(a,n);
+    quicksort(a,0,n-1);
+    print(a,n);
     return 0;
 }
+
+
 
